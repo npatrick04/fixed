101cfed04e3f825cfafa0ba08bab81ef95536601
Nick Patrick <npatrick04@gmail.com>
2016-04-11 22:59:25 -0500

    Add documentation.

--- fixed.lisp
+++ fixed.lisp
@@ -2,6 +2,8 @@
 
 (in-package #:fixed)
 
+;;; All fixed-point types have values.
+;;; This value will be shadowed by the specific definitions below.
 (defclass fp ()
   ((value)))
 
@@ -14,12 +16,24 @@
 ;;; Rule of fixed contagion...it's not.
 ;; (declaim (ftype (function (fp &rest rest) (or t nil)))
 ;; 	 f= f/= f> f>= f< f<=)
-(defgeneric f= (number &rest rest))
-(defgeneric f/= (number &rest rest))
-(defgeneric f> (number &rest rest))
-(defgeneric f>= (number &rest rest))
-(defgeneric f< (number &rest rest))
-(defgeneric f<= (number &rest rest))
+(defgeneric f= (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are equal."))
+(defgeneric f/= (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are not equal."))
+(defgeneric f> (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are in descending order."))
+(defgeneric f>= (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are in descending-or-equal order."))
+(defgeneric f< (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are in ascending order."))
+(defgeneric f<= (number &rest rest)
+  (:documentation "Determine if fixed-point values of the same
+type are in ascending-or-equal order."))
 
 (defmacro predicate (pname name raw fn)
   `(defmethod ,pname ((number ,name) &rest rest)
@@ -30,15 +44,51 @@
 		(apply #',pname next (cdr rest))))
 	 t)))
 
-(defgeneric f+ (value &rest rest))
-(defgeneric f- (value &rest rest))
-(defgeneric f* (value &rest rest))
+(defgeneric f+ (value &rest rest)
+  (:documentation "Sum all fixed-point arguments of a single type."))
+(defgeneric f- (value &rest rest)
+  (:documentation "Subtract all fixed-point arguments of a single type
+  from the first value when more than one value are provided.  If only
+  a single value is provided, negate that value."))
+(defgeneric f* (value &rest rest)
+  (:documentation "Multiply all fixed-point arguments of a single
+  type.  The first value returned is the resultant fixed-point value,
+  the second value is the residual."))
+;;; TODO f/
 (defmacro math (mname name sv-name v-name fn)
   `(defmethod ,mname ((value ,name) &rest rest)
      (,sv-name (make-instance ',name)
 	       (apply #',fn (,v-name value) (mapcar #',v-name rest)))))
 
-(defmacro defdelta (name delta &key range small)
+(defmacro defdelta (name delta &key low high small)
+  "Define a fixed point class named NAME which supports a resolution
+  of DELTA.  The class maintains the value as an
+  (INTEGER LOW-RANGE HIGH-RANGE) where the LOW-RANGE and HIGH_RANGE are
+  determined to provide an engineering-unit value within the range of low
+  to high.
+
+  When SMALL is provided, the resolution of the type is defined to
+  be exactly SMALL.  When only DELTA is provided, the resolution of
+  the type is defined to be the negative power of 2 that is no larger
+  than DELTA.
+
+  e.g. (defdelta foo 1/10) yields a SMALL value of 1/16.
+       (defdelta foo 1/10 :small 1/10) yields a SMALL value of 1/10.
+
+  This definition also produces a set of related functions and generic
+  methods for working with the NAME type.
+- MAKE-NAME: Creates a NAME type with initial value rounded to the
+  provided value. 
+- MAKE-NAME-VALUE: Creates a NAME type with internal value as
+  provided. 
+- SET-NAME: A function to set the internal value according to the
+  engineering unit value provided by rounding.
+- SET-NAME-VALUE: A function to set the internal value.
+- NAME-VALUE: Accessor (setf'able) for the internal value.
+- NAME: Accessor (setf'able) for the engineering unit value.
+
+- Predicates: f= f/= f< f<= f> f>=
+- Math operations: f+ f- f*"
   (let ((fn-name (intern (concatenate 'string
                                       "MAKE-"
                                       (symbol-name name))))
@@ -58,72 +108,76 @@
 				       "-VALUE")))
         (small (if small small
 		   (expt 2 (- (ceiling (log (/ delta) 2)))))))
-    (let ((low  (and range (round (caadr range)  small)))
-	  (high (and range (round (cadadr range) small))))
+    ;; Calculate the low and high count values
+    ;; If ranges aren't provided, substitute with '* to support its
+    ;; use in type specifiers.
+    (let ((range-p    (or low high))
+          (low-range  (if low  (ceiling low  small) '*))
+          (high-range (if high (floor   high small) '*)))
       `(progn
-	 (defclass ,name (,(if range
-			       'ordinary-ranged-fp
-			       'ordinary-fp))
-	   ((value :reader ,raw-name
-		   :writer (setf %value)
-		   :initarg :value
-		   :type ,(if range
-			      `(integer ,low ,high)
-			      `integer))))
-	 (defun ,set-raw-name (fp value)
-	   ,(if range
-		`(setf (%value fp)
-		       (the (integer ,low ,high)
-			    value))
-		`(setf (%value fp)
-		       (the integer value)))
-	   fp)
-	 (defun ,set-name (fp value)
-	   (,set-raw-name fp (round value ,small)))
-	 (defun ,fn-name (value)
-	   (,set-name (make-instance ',name) value))
-	 (defun ,fn-raw-name (value)
-	   (,set-raw-name (make-instance ',name) value))
-	 (defun ,name (fp)
-	   ;; There's got to be a better way...
-	   (declare (optimize speed))
-	   (* ,delta
-	      (round (* (the integer (,raw-name fp))
-			,small)
-		     ,delta)))
+         (defclass ,name (,(if range-p
+                               'ordinary-ranged-fp
+                               'ordinary-fp))
+           ((value :reader ,raw-name
+                   :writer (setf %value)
+                   :initarg :value
+                   :type ,(if range-p
+                              `(integer ,low-range ,high-range)
+                              `integer))))
+         (defun ,set-raw-name (fp value)
+           ,(if range-p
+                `(setf (%value fp)
+                       (the (integer ,low-range ,high-range)
+                            value))
+                `(setf (%value fp)
+                       (the integer value)))
+           fp)
+         (defun ,set-name (fp value)
+           (,set-raw-name fp (round value ,small)))
+         (defun ,fn-name (value)
+           (,set-name (make-instance ',name) value))
+         (defun ,fn-raw-name (value)
+           (,set-raw-name (make-instance ',name) value))
+         (defun ,name (fp)
+           ;; There's got to be a better way...
+           (declare (optimize speed))
+           (* ,delta
+              (round (* (the integer (,raw-name fp))
+                        ,small)
+                     ,delta)))
 
-	 (defmethod print-object ((object ,name) stream)
-	   (print-unreadable-object (object stream :type t)
-	     (format stream "~A" (,name object))))
+         (defmethod print-object ((object ,name) stream)
+           (print-unreadable-object (object stream :type t)
+             (format stream "~A" (,name object))))
 
-	 (defsetf ,name ,set-name)
-	 (defsetf ,raw-name ,set-raw-name)
+         (defsetf ,name ,set-name)
+         (defsetf ,raw-name ,set-raw-name)
 
-	 (predicate f=  ,name ,raw-name =)
-	 (predicate f/= ,name ,raw-name /=)
-	 (predicate f>  ,name ,raw-name >)
-	 (predicate f>= ,name ,raw-name >=)
-	 (predicate f<  ,name ,raw-name <)
-	 (predicate f<= ,name ,raw-name <=)
+         (predicate f=  ,name ,raw-name =)
+         (predicate f/= ,name ,raw-name /=)
+         (predicate f>  ,name ,raw-name >)
+         (predicate f>= ,name ,raw-name >=)
+         (predicate f<  ,name ,raw-name <)
+         (predicate f<= ,name ,raw-name <=)
 
-	 (math f+ ,name ,set-raw-name ,raw-name +)
-	 (math f- ,name ,set-raw-name ,raw-name -)
-	 (defmethod f* ((value ,name) &rest rest)
-	   (multiple-value-bind (quotient remainder)
-	       (round (apply #'* (,raw-name value)
-			     (mapcar (lambda (val)
-				       (* (,raw-name val) ,small))
-				     rest)))
-	     (values (,set-raw-name (make-instance ',name)
-				    quotient)
-		     (* remainder ,small))))
-	 ;; * and - are not as simple
-	 ;; v = i1 * small
-	 ;; v1 * v2 == i1 * small * i2 * small
+         (math f+ ,name ,set-raw-name ,raw-name +)
+         (math f- ,name ,set-raw-name ,raw-name -)
+         (defmethod f* ((value ,name) &rest rest)
+           (multiple-value-bind (quotient remainder)
+               (round (apply #'* (,raw-name value)
+                             (mapcar (lambda (val)
+                                       (* (,raw-name val) ,small))
+                                     rest)))
+             (values (,set-raw-name (make-instance ',name)
+                                    quotient)
+                     (* remainder ,small))))
+         ;; * and - are not as simple
+         ;; v = i1 * small
+         ;; v1 * v2 == i1 * small * i2 * small
 
-	 ',name))))
+         ',name))))
 
-(defmacro defdecimal (name digits &key range)
+(defmacro defdecimal (name digits &key low high)
   "A short-cut for defining a base-10 decimal type, that also happens
   to be printed correctly."
   (let ((delta (gensym "DELTA"))
@@ -132,7 +186,7 @@
 				       "-VALUE"))))
     `(let ((,delta (expt 10 (- ,digits))))
        (declare (type rational ,delta))
-       (defdelta ,name ,delta :range ,range :small ,delta)
+       (defdelta ,name ,delta :low ,low :high ,high :small ,delta)
        (defmethod print-object ((object ,name) stream)
 	 (print-unreadable-object (object stream :type t)
 	   (multiple-value-bind (quotient remainder)
